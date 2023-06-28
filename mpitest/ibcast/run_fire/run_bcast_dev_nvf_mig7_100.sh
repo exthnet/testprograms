d80e304461e5cd385686a16b36773af2fbbda0d1
Satoshi OHSHIMA <ohshima@cc.kyushu-u.ac.jp>
2023-06-28 17:18:59 +0900

    update mpitest

---
+++ mpitest/ibcast/run_fire/run_bcast_dev_nvf_mig7_100.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+#SBATCH -p fire
+
+date
+hostname
+module load nvhpc/23.3
+module list
+
+sudo nvidia-smi -i 1 -pm 1
+sudo nvidia-smi mig -cgi 1g.5gb,1g.5gb,1g.5gb,1g.5gb,1g.5gb,1g.5gb,1g.5gb -C
+nvidia-smi
+nvidia-smi -L
+
+export OMP_NUM_THREADS=1
+mpirun -display-devel-map -n 7 --map-by ppr:2:numa --mca btl_smcuda_use_cuda_ipc 0 ./run7.sh ./bcast_dev_nvf 100 2>&1|tee log_bcast_dev_nvf_mig7_100.txt
+
+date
